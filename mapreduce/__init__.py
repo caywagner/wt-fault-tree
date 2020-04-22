e37ca91af88eb2241aca27ea47e82d47fee663a5
Cay Wagner <cowagner@umich.edu>
2020-04-22 13:43:33 -0400

    lets not do that all at once

--- mapreduce/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Mapreduce python package.
-
-Amy Chern <chernamy@umich.edu>
-Nilay Muchhala <nilaym@umich.edu>
-"""
-
-from mapreduce.master import Master
-from mapreduce.worker import Worker
--- mapreduce/worker/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Worker python package emulating a worker node for distributed mapreduce.
-
-Amy Chern <chernamy@umich.edu>
-Nilay Muchhala <nilaym@umich.edu>
-"""
-
-from mapreduce.worker.__main__ import Worker
